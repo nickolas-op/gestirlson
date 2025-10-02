90d92658d4b8653d4fcc0b806295c868882f2714
nickolas-op <nickolas.padua@escola.pr.gov.br>
2025-10-02 09:01:54 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -55,3 +55,41 @@ sigma.addEventListener('click', zap);
 function zap (){
   alert("ta procurando o guê")
 }
+
+//executa a porra toda
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener('click', function(event){
+  event.preventDefault();
+
+  var formulario = document.querySelector("#form-adiciona");
+
+//pega os valor
+
+var nome = formulario.nome.value;
+var peso = formulario.peso.value;
+var altura = formulario.altura.value;
+var gordura = formulario.gordura.value;
+
+//cria o rai do Tr
+
+var pacienteTr = document.createElement("tr");
+
+//faz a desgraça das Td
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+imcTd.textContent = imc;
+
+});
+
+
+
