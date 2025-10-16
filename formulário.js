e05dfeb4a1f6cc71ecce4281089f3b04a707217f
nickolas-op <nickolas.padua@escola.pr.gov.br>
2025-10-16 09:02:02 -0300

    Add files via upload

--- formulário.js
+++ formulário.js
@@ -5,43 +5,56 @@ botaoAdicionar.addEventListener('click', function(event){
   event.preventDefault();
 
   var formulario = document.querySelector("#form-adiciona");
-
-//pega os valor
-
-var nome = formulario.nome.value;
-var peso = formulario.peso.value;
-var altura = formulario.altura.value;
-var gordura = formulario.gordura.value;
-
-//cria o rai do Tr
-
-var pacienteTr = document.createElement("tr");
-
-//faz a desgraça das Td
-
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
-
-//adiciona a desgraça toda
-
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calcularIMC(peso, altura);
-
-//coloca a porra toda na tela
-
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd)
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(imcTd);
+  var paciente = buscaValor(formulario);
+  var pacienteTr = criarTr(paciente);
 
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.appendChild(pacienteTr);
 
 });
+
+function buscaValor(formulario){
+
+  var paciente = {
+    nome: formulario.nome.value,
+    peso: formulario.peso.value,
+    altura: formulario.altura.value,
+    gordura: formulario.gordura.value,
+    imc: calcularIMC(formulario.peso.value, formulario.altura.value)
+  }
+  return paciente;
+}
+
+function criarTr(paciente){
+
+ //adiciona a desgraça toda
+
+  nomeTd.textContent = paciente.nome;
+  pesoTd.textContent = paciente.peso;
+  alturaTd.textContent = paciente.altura;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
+  //cria o rai do Tr
+
+  var pacienteTr = document.createElement("tr");
+
+  //faz a desgraça das Td
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+
+
+  //coloca a porra toda na tela
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd)
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  return pacienteTr;
+} 
